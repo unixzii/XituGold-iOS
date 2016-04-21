f41999e2077815a3f3c71602173b78e2980222d1
杨弘宇 <unixzii@163.com>
2016-04-21 09:11:21 +0800

    😁

---
+++ XituGold/XGConfig.swift
@@ -0,0 +1,17 @@
+//
+//  XGConfig.swift
+//  XituGold
+//
+//  Created by 杨弘宇 on 16/4/17.
+//  Copyright © 2016年 Cyandev. All rights reserved.
+//
+
+import UIKit
+
+let XGAppId = “YOUR_APP_ID”
+let XGAppKey = "YOUR_APP_KEY”
+
+let XGGlobalTintColor = UIColor(red:0.1, green:0.52, blue:0.91, alpha:1)
+
+let XGTagNames = ["全部", "Android", "设计", "前端", "iOS", "后端", "产品", "工具资源", "阅读"]
+let XGTags = ["", "android", "design", "frontend", "ios", "backend", "product", "freebie", "article"]
